db5edf0dc043320b67f2f3f838073f696655bf58
giosh99 <calgarogiosue@outlook.com>
2021-08-18 10:57:46 +0200

    first commit

---
+++ bot.js
@@ -0,0 +1,67 @@
+        //  __   __  ___        ___
+        // |__) /  \  |  |__/ |  |  
+// |__) \__/  |  |  \ |  |  
+
+// This is the main file for the prova bot.
+
+// Import Botkit's core features
+const { Botkit } = require('botkit');
+const { BotkitCMSHelper } = require('botkit-plugin-cms');
+const { WebAdapter } = require('botbuilder-adapter-web');
+// Import a platform-specific adapter for web.
+const { MongoDbStorage } = require('botbuilder-storage-mongodb');
+
+// Load process.env values from .env file
+require('dotenv').config();
+
+let storage = null;
+if (process.env.MONGO_URI) {
+    storage = mongoStorage = new MongoDbStorage({
+        url : process.env.MONGO_URI,
+    });
+}
+
+
+const adapter = new WebAdapter({});
+
+
+const controller = new Botkit({
+    webhook_uri: '',
+
+    adapter: adapter,
+
+    storage
+});
+
+if (process.env.CMS_URI) {
+    controller.usePlugin(new BotkitCMSHelper({
+        uri: process.env.CMS_URI,
+        token: process.env.CMS_TOKEN,
+    }));
+}
+
+controller.ready(() => {
+
+    // load traditional developer-created local custom feature modules
+    controller.loadModules(__dirname + '/features');
+
+    /* catch-all that uses the CMS to trigger dialogs */
+    if (controller.plugins.cms) {
+        controller.on('message,direct_message', async (bot, message) => {
+            let results = false;
+            results = await controller.plugins.cms.testTrigger(bot, message);
+
+            if (results !== false) {
+                // do not continue middleware!
+                return false;
+            }
+        });
+    }
+
+});
+
+
+
+
+
+
---
+++ features/move.js
@@ -0,0 +1,114 @@
+/* esempio */
+const { BotkitConversation } = require("botkit");
+
+module.exports = function(controller) {
+    const move = new BotkitConversation('move', controller);
+
+    //move.addAction('move');
+  //  move.addMessage('Benvenuto nella sessione MOVE {{vars.name}}','move');
+  move.addAction('move');
+  move.addQuestion({
+      text: 'Cosa desideri fare?',
+      type: 'question',
+      quick_replies: [
+          {
+          content_type: 'text',
+          title: 'Mostrami il video di un esercizio',
+          payload: 'show_video'
+      },
+      {
+          content_type: 'text',
+          title: 'Voglio inviare il video di un esercizio',
+          payload: 'send_video'
+      },
+      {
+          content_type: 'text',
+          title: 'Voglio parlare con il mio coach',
+          payload: 'talk_to_coach'
+      }
+      ],
+  },
+      [
+          {
+              pattern: 'show_video',
+              type: 'string',
+              handler:  async(response_text, controller, bot) => {
+                  controller.gotoThread('video');
+              }
+          },
+          {
+              pattern: 'talk_to_coach',
+              type: 'string',
+              handler: async(response_text, controller, bot) => {
+                  try {return await bot.say('Hai scelto l\'opzione coach');}
+                  catch(error) {console.error(error);}
+              }
+          }
+      ]
+     , {key: 'choice'}, 'move', 'video');
+     
+    move.addAction('video');
+    move.addQuestion({
+        text:'Aspe, prima che guardi a caso, preferisci qualcosa in particolare?',
+        type:'question',
+        quick_replies: [
+            {
+                content_type: 'text',
+                title: 'Video di squat',
+                payload: 'squat_video'
+            },
+            {
+                content_type: 'text',
+                title: 'Video di addominali su panca piana',
+                payload: 'addominali_panca_video'
+            },
+            {
+                content_type: 'text',
+                title: 'Video di trazioni sopra un albero',
+                payload: 'trazioni_video'
+            },
+        ]
+    },[
+        {
+            pattern: 'squat_video',
+            type:'string',
+            handler: async(response_text, controller, bot) => {
+                bot.say({
+                    text: 'Hai scelto squat! Dammi un secondo e arrivo con il tuo video!',
+                    type: 'choice',
+                });
+            }
+        },
+        {
+            pattern: 'addominali_panca_video',
+            type:'string',
+            handler: async(response_text, controller, bot) => {
+                bot.say({
+                    text: 'Hai scelto addominali! Dammi un sec e arrivo!',
+                    type: 'choice',
+                });
+            }
+        },
+        {
+            pattern: 'trazioni_video',
+            type:'string',
+            handler: async(response_text, controller, bot) => {
+                bot.say({
+                    text: 'Hai scelto trazioni! Dammi un sec e arrivo!',
+                    type: 'choice',
+                });
+            }
+        }
+    ], {key: 'sec_choice'}, 'video');
+
+    controller.addDialog(move);
+
+    move.before('move', async(controller, bot) => {
+        // set a variable here that can be used in the message template
+        controller.setVar('name','Giosuè');
+       });
+
+    controller.hears('move','message,direct_message', async(bot, message) => {
+        await bot.beginDialog('move');
+    });
+}
